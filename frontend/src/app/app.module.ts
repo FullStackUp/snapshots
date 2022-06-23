322efc7a75c971796d94bee4702ff2015973687c
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-06-23 21:25:14 +0200

    Add component  header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { MyPictureComponent } from './my-picture/my-picture.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MyPictureListComponent } from './my-picture-list/my-picture-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, MyPictureComponent, MyPictureListComponent],
+  declarations: [AppComponent, MyPictureComponent, MyPictureListComponent, HeaderComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
