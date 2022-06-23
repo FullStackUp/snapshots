addf38a37f48686173afe524dd89186740a3fa17
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-06-23 21:05:32 +0200

    Restructure the application

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { MyPictureComponent } from './my-picture/my-picture.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MyPictureListComponent } from './my-picture-list/my-picture-list.component';
 
 @NgModule({
-  declarations: [AppComponent, MyPictureComponent],
+  declarations: [AppComponent, MyPictureComponent, MyPictureListComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
